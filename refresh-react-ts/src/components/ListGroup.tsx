ef79e00f95bd492b8772af669c60d51d6cdb276b
alexiworld <alexiworld@yahoo.com>
2025-02-11 06:07:40 -0800

    Replace static <li> texts with values from a list. Note key attr is to allow React to rerender parts of the list

--- refresh-react-ts/src/components/ListGroup.tsx
+++ refresh-react-ts/src/components/ListGroup.tsx
@@ -2,18 +2,18 @@ import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  
-  items.map(item => <li>{item}</li>)
-  
+
+  items.map((item) => <li>{item}</li>);
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
